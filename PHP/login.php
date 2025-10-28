22b5e8ad588487379dd5bbea614b9da9f47b1655
bhumiip06 <bhumikapimoli87@gmail.com>
2025-10-28 23:20:33 +0530

    php logic for role based login

---
+++ PHP/login.php
@@ -0,0 +1,43 @@
+<?php
+    session_start();
+    include 'connection.php';
+
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+
+        $query="select* from users where email=?";
+        $stmt=$con->prepare($query);
+        $stmt->bind_param("s",$email);
+        $stmt->execute();
+        $result=$stmt->get_result();
+
+        if($result->num_rows==1){
+            $user=$result->fetch_assoc();
+
+            if($password==$user['password']){
+                $_SESSION['user_id']=$user['user_id'];
+                $_SESSION['role']=$user['role'];
+                $_SESSION['username']=$user['username'];
+
+                if($user['role']=='admin'){
+                    header("Location: admin_dashboard.php");
+                }
+                elseif($user['role']=='doctor'){
+                    header("Location: doctor_dasboard.php");
+                }
+                else{
+                    header("Location: patient_dashboard.php");
+                }
+                exit();
+            }
+            else{
+                echo"Invalid password";
+            }
+        }
+        else{
+            echo"No user found with this email";
+        }
+    }
+
+?>
