18620ddec43b3dab92f4ee2fc49c23b5ddedcbdd
bhumiip06 <bhumikapimoli87@gmail.com>
2025-11-01 21:20:41 +0530

    cancel appointment feature in appointments module php logic

---
+++ Appointments/cancel_appointment.php
@@ -0,0 +1,9 @@
+<?php
+include '../PHP/connection.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $conn->query("UPDATE appointments SET status='Cancelled' WHERE appointment_id='$id'");
+    echo "<script>alert('Appointment cancelled successfully!'); window.location='view_appointments.php';</script>";
+}
+?>
